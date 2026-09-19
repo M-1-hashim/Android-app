500249a48405490a34806159c3d07336bbffb290
M-1-hashim <mohammadhashimgholami8@gmail.com>
2026-09-20 01:21:00 +0430

    Add files via upload

---
+++ capacitor.config.js
@@ -0,0 +1,10 @@
+const { CapacitorConfig } = require('@capacitor/cli');
+
+const config = {
+  appId: 'com.lumix.luxefinance',
+  appName: 'Luxe Finance',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+module.exports = config;
